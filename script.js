0888b1eb0f480f32543081e449b046a472184758
EncisoMath <enciso.math@gmail.com>
2024-09-01 10:34:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -174,15 +174,15 @@ async function buscar() {
                     resultado.innerHTML = `
                         <h1>Resultados</h1>
                         <div class="resultado-item">
-                            <span style="color: orange;">Alumno: </span>
+                            <span style="color: orange;">Alumno:  </span>
                             <span>${NOMBRE}</span>
                         </div>
                         <div class="resultado-item">
-                            <span style="color: orange;">Sede: </span>
+                            <span style="color: orange;">Sede:  </span>
                             <span>${SEDE}</span>
                         </div>
                         <div class="resultado-item">
-                            <span style="color: orange;">Grado: </span>
+                            <span style="color: orange;">Grado:  </span>
                             <span>${GRADO}</span>
                         </div>
                         <hr style="border: 3px solid red; margin: 20px 0; width: 100%;">
