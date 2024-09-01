5ec3221d5ceac677280cb1ce6313bde9161f9b5b
EncisoMath <enciso.math@gmail.com>
2024-09-01 08:24:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ async function buscar() {
                                         <td style="padding: 8px; text-align: center;">
                                             <div style="display: flex; flex-direction: column; align-items: center;">
                                                 <img src="https://raw.githubusercontent.com/EncisoMath/Prueba/main/estadistica.png" style="width: 60px; height: 60px;">
-                                                <span>${asignatura.nombre}</span>
+                                                <span>Hola ${asignatura.nombre}</span>
                                             </div>
                                         </td>
                                         <td style="padding: 8px;">
